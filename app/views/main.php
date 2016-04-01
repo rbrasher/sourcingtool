98dcddd08903c56bd330188a74bb063531cc2cb4
Ron Brasher <ron.sightglass@gmail.com>
2016-04-01 13:40:09 -0700

    added hyperlinks that I forgot in dashboard

--- app/views/main.php
+++ app/views/main.php
@@ -29,10 +29,10 @@
                             </div>
                         </div>
                     </div>
-                    <a href="#">
+                    <a href="<?php echo base_url();?>products">
                         <div class="panel-footer">
                             <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <span class="pull-right"><span class="glyphicon glyphicon-circle-arrow-right"></span></span>
                             <div class="clearfix"></div>
                         </div>
                     </a>
@@ -51,10 +51,10 @@
                             </div>
                         </div>
                     </div>
-                    <a href="#">
+                    <a href="<?php echo base_url();?>products/production">
                         <div class="panel-footer">
                             <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <span class="pull-right"><span class="glyphicon glyphicon-circle-arrow-right"></span></span>
                             <div class="clearfix"></div>
                         </div>
                     </a>
@@ -73,10 +73,10 @@
                             </div>
                         </div>
                     </div>
-                    <a href="#">
+                    <a href="<?php echo base_url();?>launchcalendar" target="_blank">
                         <div class="panel-footer">
                             <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <span class="pull-right"><span class="glyphicon glyphicon-circle-arrow-right"></span></span>
                             <div class="clearfix"></div>
                         </div>
                     </a>
@@ -95,10 +95,10 @@
                             </div>
                         </div>
                     </div>
-                    <a href="#">
+                    <a href="<?php echo base_url();?>products/production">
                         <div class="panel-footer">
                             <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <span class="pull-right"><span class="glyphicon glyphicon-circle-arrow-right"></span></span>
                             <div class="clearfix"></div>
                         </div>
                     </a>
